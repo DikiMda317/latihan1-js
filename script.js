358c29813e9209593991bbf133c03b61b79a9a9a
diki <dikiweb317.com>
2022-01-16 13:38:56 +0700

    aplikasi angkot selesai

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ function Angkot(nama, trayek, penumpang, kas) {
     this.penumpang.push(namaPenumpang);
     return this.penumpang;
   };
-}
 
+  this.penumpangTurun = function (namaPenumpang, bayar) {
+    if (this.penumpang.length === 0) {
+      alert("penumpang masih kosong !!");
+    }
+
+    for (var i = 0; this.penumpang.length; i++) {
+      if (this.penumpang[i] == namaPenumpang) {
+        this.penumpang[i] = undefined;
+        this.kas += bayar;
+        return this.penumpang;
+      }
+    }
+  };
+}
 var angkot1 = new Angkot("sandika", ["cicahem", "cibiru"], [], 0);
